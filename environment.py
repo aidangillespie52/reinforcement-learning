f366e8a3dec3f7bb4c4b20e36f195608cefbbaf1
aidangillespie52 <142521298+aidangillespie52@users.noreply.github.com>
2025-08-28 22:46:13 -0500

    added get intial state as abstract method for env

--- environment.py
+++ environment.py
@@ -4,12 +4,14 @@ class MyEnv(ABC):
     @property
     @abstractmethod
     def Actions(self):
-        """Return the Enum of valid actions"""
         pass
     
     @abstractmethod
     def get_actions(self):
-        """Return iterable of valid actions"""
+        pass
+    
+    @abstractmethod
+    def get_initial_state(self):
         pass
     
     @abstractmethod
